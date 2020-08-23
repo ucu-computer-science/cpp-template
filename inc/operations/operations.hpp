c347015ee1a6588cada07ad77d6c167c8392f090
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2020-08-23 19:45:34 +0300

    Add sample project

---
+++ inc/operations/operations.hpp
@@ -0,0 +1,9 @@
+#ifndef ADDER_OPERATIONS_HPP
+#define ADDER_OPERATIONS_HPP
+
+namespace operations
+{
+    int add(int, int);
+}
+
+#endif //ADDER_OPERATIONS_HPP
---
+++ src/operations.cpp
@@ -0,0 +1,6 @@
+#include "operations/operations.hpp"
+
+int operations::add(int a, int b)
+{
+    return a + b;
+}
