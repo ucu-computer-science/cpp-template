c0ae6c60f643a88ac918b36b35f6c7e9ca8cfedb
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2020-10-06 16:20:56 +0300

    Add newlines around PVS-Studio message

--- cmake/defaults/PVSStudio.cmake
+++ cmake/defaults/PVSStudio.cmake
@@ -6,5 +6,5 @@ if (ENABLE_PVS_STUDIO)
                           ANALYZE ${ALL_TARGETS}
                           LOG target.err)
 else ()
-    message(NOTICE "Consider using PVS-Studio with `-DENABLE_PVS_STUDIO=ON` flag or Windows GUI application.")
+    message(NOTICE "\nConsider using PVS-Studio with `-DENABLE_PVS_STUDIO=ON` flag or Windows GUI application.\n")
 endif ()
