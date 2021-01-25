1de993a26215bfc7d493da439633619088ad1336
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2021-01-25 21:55:43 +0200

    Remove PVS-Studio comment from header

--- inc/arithmetic/arithmetic.hpp
+++ inc/arithmetic/arithmetic.hpp
@@ -1,6 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
 #ifndef ADDER_ARITHMETIC_HPP
 #define ADDER_ARITHMETIC_HPP
 
